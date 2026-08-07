8a5955eb6372356a30d0af38a84fae7bd8a97f62
oraculotw <107263071+oraculotw@users.noreply.github.com>
2026-08-07 09:05:18 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+//Botões de acessibilidade
+document.addEventListener("DOMContentLoaded, () => {
+    
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+//aumenta a fonte
+
+let tamanhoAtualFonte = 100;
+
+//função alto contraste
+
+btnContraste.addEventListener("click", () =>{
+    
+document.body.classList.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela
+
+const ativo = document.body.classList.contains("alto-contraste");
+btnContraste.setAttribute("aria-pressed", ativo);
+
+)
+
+
+}
