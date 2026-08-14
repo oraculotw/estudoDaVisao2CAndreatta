cc018adc894226abeb3443bcc514678c7dae6b51
oraculotw <107263071+oraculotw@users.noreply.github.com>
2026-08-14 08:46:35 -0300

    ok

--- script.js
+++ script.js
@@ -20,7 +20,22 @@ document.body.classList.toogle("alto-contraste");
 const ativo = document.body.classList.contains("alto-contraste");
 btnContraste.setAttribute("aria-pressed", ativo);
 
-)
+)};
 
+//função aumentar o texto
 
-}
+btnAumentar.addEventListener("click", ()=>{
+    if (tamanhoAtualFonte < 150){
+        tamanhoAtualFonte += 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+});
+
+//função diminuir texto;
+
+btnDiminuir.addEventListener("click", ()=> {
+    if(tamanhoAtualFonte > 90){
+        tamanhoAtualFonte -= 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+})
